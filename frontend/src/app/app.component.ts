9167ff721dd4060b78f697551012f0cae55393c9
Melike Sena Çakır <melikesennaa@gmail.com>
2024-11-22 03:27:55 +0300

    searchbar created

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SearchbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

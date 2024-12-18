e1be0d3252131f55474d635595f6a0f1b0640445
Melike Sena Çakır <melikesennaa@gmail.com>
2024-12-18 19:40:23 +0300

    gitignore updated

---
+++ backend/api/models/__init__.py
@@ -0,0 +1,4 @@
+from ....model import Recipe, SearchHistory  # Modellerinizi burada içe aktarın
+from ....model import search_history_recipes  # Ara tabloyu da ekleyin
+
+__all__ = ['Recipe', 'SearchHistory', 'search_history_recipes']  # Dışarıya aktarılacak modeller

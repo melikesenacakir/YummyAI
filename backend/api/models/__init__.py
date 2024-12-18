14821c9851f3b1073986c145f8aae7e3ba2e33a8
Melike Sena Çakır <melikesennaa@gmail.com>
2024-12-18 19:41:07 +0300

    bug deleted

--- backend/api/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from ....model import Recipe, SearchHistory  # Modellerinizi burada içe aktarın
-from ....model import search_history_recipes  # Ara tabloyu da ekleyin
-
-__all__ = ['Recipe', 'SearchHistory', 'search_history_recipes']  # Dışarıya aktarılacak modeller
